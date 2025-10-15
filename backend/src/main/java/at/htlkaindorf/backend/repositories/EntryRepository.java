2e9637a46a03b196fcf7c437194e6b4f927bad73
clemens4554 <94311479+clemens4554@users.noreply.github.com>
2025-10-15 22:20:29 +0200

    changed getEntries Route

--- backend/src/main/java/at/htlkaindorf/backend/repositories/EntryRepository.java
+++ backend/src/main/java/at/htlkaindorf/backend/repositories/EntryRepository.java
@@ -5,6 +5,12 @@ import at.htlkaindorf.backend.ids.EntryId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface EntryRepository extends JpaRepository<Entry, EntryId> {
+
+    List<Entry> findAllByEntryId_EntryDateAndTennisCourt_TennisCourtId(LocalDate entryDate, Long tennisCourtId);
+
 }
